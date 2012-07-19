c7fe830ddf409a00a09b08abf71008b3dfe1bfe1
Eugene Russkikh <eugene@russkikh.org.ua>
2012-07-19 16:41:28 +0300

    fix again

--- lib/yr/ocean_forecast.rb
+++ lib/yr/ocean_forecast.rb
@@ -20,8 +20,8 @@ module Yr
       ocean_forecast = {}
 
       doc.xpath('//mox:Forecasts/mox:forecast/metno:OceanForecast').each do |forecast|
-        time = Time.xmlschema(forecast.xpath('mox:validTime/gml:TimePeriod/gml:begin').first.children.first.to_s)
-        ocean_forecast[time] = forecast.xpath('mox:seaSurfaceHeight').first.children.first.to_s
+        time = Time.xmlschema(forecast.xpath('mox:validTime/gml:TimePeriod/gml:begin').first.try(:children).try(:first).try(:to_s))
+        ocean_forecast[time] = forecast.xpath('mox:seaSurfaceHeight').first.try(:children).try(:first).try(:to_s))
       end
 
       ocean_forecast
